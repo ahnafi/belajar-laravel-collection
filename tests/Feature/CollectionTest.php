cd6315a98720f8ff699d0ae3ddac23ccac3135f0
ahnafi <devsulthon@gmail.com>
2025-01-29 13:42:38 +0700

    learn : Manipulasi Collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -22,4 +22,25 @@ class CollectionTest extends TestCase
         }
     }
 
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        self::assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+
+        $pop = $collection->pop();
+        self::assertEquals(3, $pop);
+        self::assertEqualsCanonicalizing([1, 2], $collection->all());
+
+        $prepend = $collection->prepend(0);
+        self::assertEquals([0, 1, 2], $prepend->all());
+//        self::assertEqualsCanonicalizing([1, 2], $collection->all());
+
+        $pull = $prepend->pull(2);
+        self::assertEquals(2, $pull);
+
+        $put = $collection->put("a", 1);
+        self::assertEquals([0, 1, "a" => 1], $put->all());
+    }
+
 }
